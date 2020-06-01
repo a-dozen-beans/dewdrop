a4707e89a265502df839d9dfb06b3e0694cfef1b
a-dozen-beans <agnieszka.holun@gmail.com>
2020-06-01 18:11:31 +0200

    Change urls

--- dewdrop/dewdrop/urls.py
+++ dewdrop/dewdrop/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
+from dewdrop import urls as app_urls
 
 urlpatterns = [
     path('recruitment/', include('recruitment.urls')),
     path('admin/', admin.site.urls),
+    path('', include(app_urls)),
 ]
 
 admin.site.site_header = "Recruitment administration"
